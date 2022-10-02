caf9fd05ab5c4b0ab5c73c311f1b8c2a38b26a0b
Екатерина Устинова <dadsda@inbox.ru>
2022-10-02 19:37:54 +0700

    add project

---
+++ src/test/java/ru/netology/web/page/LoginPageV.java
@@ -0,0 +1,14 @@
+package ru.netology.web.page;
+
+import ru.netology.web.data.DataHelper;
+
+import static com.codeborne.selenide.Selenide.$x;
+
+public class LoginPageV {
+    public VerificationPage validLogin(DataHelper.AuthInfo info) {
+        $x("//*[@data-test-id=\"login\"]//self::input").setValue(info.getLogin());
+        $x("//*[@data-test-id=\"password\"]//self::input").setValue(info.getPassword());
+        $x("//*[@data-test-id=\"action-login\"]").click();
+        return new VerificationPage();
+    }
+}
---
+++ src/test/java/ru/netology/web/page/MoneyTransferPage.java
@@ -0,0 +1,24 @@
+package ru.netology.web.page;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.SelenideElement;
+import ru.netology.web.data.DataHelper;
+import static com.codeborne.selenide.Selenide.$x;
+
+public class MoneyTransferPage {
+    private SelenideElement transferAmount = $x("//*[@data-test-id=\"amount\"]//self::input");
+    private SelenideElement fromField = $x("//*[@data-test-id=\"from\"]//self::input");
+    private SelenideElement transferButton = $x("//*[@data-test-id=\"action-transfer\"]");
+    private SelenideElement errorMessage = $x("//*[@data-test-id=\"error-notification\"]");
+
+    public DashboardPage moneyTransfer(DataHelper.CardInfo from, String amountToTransfer) {
+        transferAmount.setValue(amountToTransfer);
+        fromField.setValue(from.getNumber());
+        transferButton.click();
+        return new DashboardPage();
+    }
+
+    public void getError() {
+        errorMessage.shouldBe(Condition.visible);
+    }
+}
